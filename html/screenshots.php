d2b782474a385fac382cededa0c373e7655de2ba
Bruce Merry <bmerry@gmail.com>
2009-03-25 20:35:58 +0000

    Update screenshots

--- html/screenshots.php
+++ html/screenshots.php
@@ -23,19 +23,36 @@ else
     <a href="screenshots.php">[Screenshots]</a>
     </p>
     <h1>Screenshots</h1>
-    <dl>
-        <dt>Statistics display</dt>
-        <dd><img src="images/stats.png" alt="Demo of the statistics display" /></dd>
 
-        <dt>GUI debugger</dt>
-        <dd><img src="images/gldb-gui1.png" alt="gldb-gui state" /></dd>
-        <dd><img src="images/gldb-gui2.png" alt="gldb-gui texture display" /></dd>
-        <dd><img src="images/gldb-gui3.png" alt="gldb-gui shader display" /></dd>
-        <dd><img src="images/gldb-gui4.png" alt="gldb-gui backtrace" /></dd>
-    </dl>
-    <hr />
-    <p>
-    <a href="http://sourceforge.net/projects/bugle"><img src="http://sflogo.sourceforge.net/sflogo.php?group_id=110905&amp;type=12" width="120" height="30" border="0" alt="Get BuGLe at SourceForge.net. Fast, secure and Free Open Source software downloads" /></a>
+    <ol>
+        <li><a href="#statistics">Statistics</a></li>
+        <li>Debugger</li>
+        <li><ol>
+            <li><a href="#gldb-gui-state">State</a></li>
+            <li><a href="#gldb-gui-buffers">Buffers</a></li>
+            <li><a href="#gldb-gui-textures">Textures</a></li>
+            <li><a href="#gldb-gui-framebuffers">Framebuffers</a></li>
+            <li><a href="#gldb-gui-shaders">Shaders</a></li>
+            <li><a href="#gldb-gui-backtrace">Backtrace</a></li>
+            <li><a href="#gldb-gui-breakpoints">Breakpoints</a></li>
+        </ol></li>
+    </ol>
+
+    <h2><a name="statistics">Statistics display</a></h2>
+
+    <img src="images/stats.png" alt="Demo of the statistics display" />
+
+    <h2><a name="gldb-gui">GUI Debugger</a></h2>
+
+    <p /><a name="gldb-gui-state"><img src="images/gldb-gui-state.png" alt="gldb-gui state view" /></a>
+    <p /><a name="gldb-gui-buffers"><img src="images/gldb-gui-buffers.png" alt="gldb-gui buffers view" /></a>
+    <p /><a name="gldb-gui-textures"><img src="images/gldb-gui-textures.png" alt="gldb-gui textures view" /></a>
+    <p /><a name="gldb-gui-framebuffers"><img src="images/gldb-gui-framebuffers.png" alt="gldb-gui framebuffers view" /></a>
+    <p /><a name="gldb-gui-shaders"><img src="images/gldb-gui-shaders.png" alt="gldb-gui shaders view" /></a>
+    <p /><a name="gldb-gui-backtrace"><img src="images/gldb-gui-backtrace.png" alt="gldb-gui backtrace view" /></a>
+    <p /><a name="gldb-gui-breakpoints"><img src="images/gldb-gui-breakpoints.png" alt="gldb-gui breakpoints view" /></a>
+
+    <p /><a href="http://sourceforge.net/projects/bugle"><img src="http://sflogo.sourceforge.net/sflogo.php?group_id=110905&amp;type=12" width="120" height="30" border="0" alt="Get BuGLe at SourceForge.net. Fast, secure and Free Open Source software downloads" /></a>
 <?php
 if (file_exists("/home/virtual/opengl.org/var/www/html/sdk/inc/sdk_footer.txt"))
     include("/home/virtual/opengl.org/var/www/html/sdk/inc/sdk_footer.txt");
