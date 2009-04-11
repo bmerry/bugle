6f06ed9cf8fcdf317c2cd7de127156c7e9d6b7e2
Bruce Merry <bmerry@gmail.com>
2009-04-11 15:46:51 +0000

    Update paths in websync script

--- scripts/websync.sh
+++ scripts/websync.sh
@@ -3,5 +3,5 @@ if ! [ -f html/index.php ]; then
   echo "Please run from top-level directory" 1>&2
   exit 1
 fi
-rsync -v -a --copy-links --delete --delete-after --progress --stats --exclude=.svn --exclude=documentation --exclude=snapshots --exclude='sdk_*.txt' html/ $HOME/devel/bugle-oglsdk
-xsltproc --xinclude --stringparam "base.dir" "$HOME/devel/bugle-oglsdk/documentation/" doc/DocBook/xhtml-chunk-php.xsl doc/DocBook/bugle.xml
+rsync -v -a --copy-links --delete --delete-after --progress --stats --exclude=.svn --exclude=documentation --exclude=snapshots --exclude='sdk_*.txt' html/ $HOME/devel/bugle/oglsdk
+xsltproc --xinclude --stringparam "base.dir" "$HOME/devel/bugle/oglsdk/documentation/" doc/DocBook/xhtml-chunk-php.xsl doc/DocBook/bugle.xml
