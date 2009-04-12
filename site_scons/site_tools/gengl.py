22d6bf8a66ce72c30a2a67ece3d7835c976e00b6
Bruce Merry <bmerry@gmail.com>
2009-04-12 21:44:43 +0000

    Move more work into tools

--- site_scons/site_tools/gengl.py
+++ site_scons/site_tools/gengl.py
@@ -1,7 +1,35 @@
 #!/usr/bin/env python
 from SCons import *
+from subprocess import *
+import re
+
+def find_header(env, name):
+    # TODO: -I should come from CPPPATH
+    sp = Popen([env['CC'], '-E', '-I.', '-I' + env['topbuilddir'].path, '-'],
+            stdin = PIPE, stdout = PIPE, stderr = PIPE)
+    (out, err) = sp.communicate('#include <' + name + '>\n')
+    errcode = sp.wait()
+    if errcode != 0:
+        raise NameError, err.rstrip("\r\n")
+
+    s = re.compile(r'^# \d+ "(.*' + name + r')"', re.M)
+    matches = s.search(out)
+    if matches is not None:
+        return matches.group(1)
+
+    # Preprocessor didn't find it, fall back and try some known paths
+    file = env.FindFile(name, [
+        '/usr/include',
+        '/usr/X11R6/include',
+        '/usr/local/include',
+        '/usr/include/w32api'])
+    if file is not None:
+        return file
+
+    raise NameError, 'Could not find ' + name
 
 def alias_emitter(target, source, env):
+    # Make sure the generator is treated as a source for dependency purposes
     source.append('#source/gengl/gengl.perl')
     return target, source
 
@@ -16,7 +44,9 @@ def generate(env, **kw):
     alias_builder = env.Builder(
             generator = alias_generator,
             emitter = alias_emitter)
-    env.Append(BUILDERS = {'BudgieAlias': alias_builder})
+    env.Append(
+            BUILDERS = {'BudgieAlias': alias_builder},
+            find_header = find_header)
 
 def exists(env):
     return 1
