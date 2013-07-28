57acbc924b3a6dbc9235635cc37545f4d4655345
Bruce Merry <bmerry@gmail.com>
2013-07-28 15:17:27 +0000

    Rewrite genglxml.py to use the XML parse tree directly

--- src/gengl/genglxml.py
+++ src/gengl/genglxml.py
@@ -1,174 +1,149 @@
 #!/usr/bin/env python3
-
-"""
-Generate header and C files for BuGLe from the OpenGL extension registry.
-
-The following data are emitted (depending on command-line options):
-  - apitables.h:
-    - #defines for each extension (versions are considered to be extensions)
-    - count of the number of extensions
-    - declarations for the data in apitables.c
-  - apitables.c:
-    - list of extensions in each extension group (set of extensions all providing
-      some piece of functionality)
-    - for each extension:
-        - the version name string (or NULL for non-version extensions)
-        - the extension name string
-        - the group it belongs to
-        - the API block it belongs to.
-    - for each function:
-        - the extension it belongs to.
-    - for each enum:
-        - the list of extensions that define it.
-    - for each API block
-        - the enums, with value, canonical name, and the list of extensions that define it
-        - the number of enums
-  - alias.bc:
-    - list of aliases between functions with different names but the
-      same signatures and semantics.
-
-"""
-
 import sys
 import os.path
 import re
+from collections import OrderedDict
 from optparse import OptionParser
 from textwrap import dedent
-
-apis_path = os.path.abspath(__file__)
-apis_path = os.path.dirname(os.path.dirname(os.path.dirname(apis_path)))
-apis_path = os.path.join(apis_path, 'khronos-api')
-sys.path.insert(0, apis_path)
-import reg
+from lxml import etree
 import genglxmltables
 
-def load_function_ids(filename):
-    func_re = re.compile('^#define FUNC_(\w+)\s+(\d+)')
-    functions = []
-    with open(filename, 'r') as h:
-        for line in h:
-            match = func_re.match(line)
-            if match:
-                name = match.group(1)
-                fid = int(match.group(2))
-                if fid != len(functions):
-                    raise ValueError("Expected function ID of " + len(functions) + " but got " + fid)
-                functions.append(name)
-    return functions
+class Enum:
+    def __init__(self, elem):
+        self.elem = elem
+        self.type = elem.get('type', '')
+        self.name = elem.get('name')
+        self.extensions = []
 
-def feature_key(feature):
-    '''
-    Generate a tuple which can be used to sort extensions. Extensions
-    are sorted into the order:
-     - versions, by decreasing version number
-     - Khronos-approved extensions (ARB/KHR/OES)
-     - multi-vendor extensions (EXT)
-     - vendor extensions
-     - extension groups
-    '''
-    if (feature.elem.tag == 'feature'):
-        return (0, -float(feature.number))
-    elif (feature.category == 'ARB' or
-          feature.category == 'KHR' or
-          feature.category == 'OES'):
-        return (1, feature.name)
-    elif (feature.category == 'EXT'):
-        return (2, feature.name)
-    else:
-        return (3, feature.name)
+class EnumValue:
+    def __init__(self, value):
+        self.value = value
+        self.enums = []
 
-def sort_and_save(generator):
-    def run(features):
-        reg.regSortFeatures(features)
-        generator.features = sorted(features, key = feature_key)
-    return run
+class Function:
+    def __init__(self, elem):
+        self.elem = elem
+        self.name = elem.find('proto/name').text
+        self.extensions = []
 
-class NullOutputGenerator:
-    """
-    Provides the functions expected by the registry class, with empty
-    implementations.
-    """
-    def __init__(
-            self,
-            errFile = sys.stderr,
-            warnFile = sys.stderr,
-            diagFile = sys.stdout):
-        pass
+class Extension:
+    def __init__(self, elem, api):
+        self.elem = elem
+        self.name = elem.get('name')
+        if elem.tag == 'feature':
+            self.number = self.elem.get('number')
+            self.category = 'VERSION'
+        else:
+            self.category = self.name.split('_', 2)[1]
+            self.number = "0"
 
-    def beginFile(self, genOpts):
-        pass
+        self.enums = []
+        self.functions = []
+        for require_elem in elem.iterfind('require'):
+            if api.matchRequire(require_elem):
+                for enum_elem in require_elem.iterfind('enum'):
+                    name = enum_elem.get('name')
+                    # Need the test because bit values etc are excluded
+                    if name in api.enums:
+                        enum = api.enums[name]
+                        enum.extensions.append(self)
+                        self.enums.append(enum)
+                for command_elem in require_elem.iterfind('command'):
+                    name = command_elem.get('name')
+                    function = api.functions[name]
+                    function.extensions.append(self)
+                    self.functions.append(function)
 
-    def endFile(self):
-        pass
+    def key(self):
+        '''
+        Generate a tuple which can be used to sort extensions. Extensions
+        are sorted into the order:
+         - versions, by decreasing version number
+         - Khronos-approved extensions (ARB/KHR/OES)
+         - multi-vendor extensions (EXT)
+         - vendor extensions
+         - extension groups
+        '''
+        if (self.category == 'VERSION'):
+            return (0, -float(self.number))
+        elif (self.category == 'ARB' or
+              self.category == 'KHR' or
+              self.category == 'OES'):
+            return (1, self.name)
+        elif (self.category == 'EXT'):
+            return (2, self.name)
+        else:
+            return (3, self.name)
 
-    def beginFeature(self, interface, emit):
-        pass
+class API:
+    def __init__(self, filename):
+        self.tree = etree.parse(filename)
+        self.enums = dict()
+        self.enum_values = dict()
+        self.functions = dict()
+        self.extensions = OrderedDict()
+        tree = etree.parse(filename)
+        root = tree.getroot()
+        for enums_elem in root.iterfind('enums'):
+            if enums_elem.get('type') != 'bitmask' and 'group' not in enums_elem:
+                for enum_elem in enums_elem.iterfind('enum'):
+                    enum = Enum(enum_elem)
+                    value = int(enum_elem.get('value'), 0)
+                    if value not in self.enum_values:
+                        self.enum_values[value] = EnumValue(value)
+                    enum.value = self.enum_values[value]
+                    enum.value.enums.append(enum)
+                    self.enums[enum.name] = enum
 
-    def endFeature(self):
-        pass
+        for command_elem in root.iterfind('commands/command'):
+            function = Function(command_elem)
+            self.functions[function.name] = function
 
-    def genType(self, typeinfo, name):
-        pass
+        extensions = []
+        for feature_elem in root.iterfind('feature'):
+            if self.useExtension(feature_elem):
+                extensions.append(Extension(feature_elem, self))
+        for extension_elem in root.iterfind('extensions/extension'):
+            if self.useExtension(extension_elem):
+                extensions.append(Extension(extension_elem, self))
+        extensions.sort(key = Extension.key) # Sort by key, to insert into OrderedDict
+        for extension in extensions:
+            self.extensions[extension.name] = extension
 
-    def genEnum(self, enuminfo, name):
-        pass
+        # Prune enums and functions that don't apply in this API/profile
+        self.enums = {x[0]: x[1] for x in self.enums.items() if x[1].extensions}
+        self.functions = {x[0]: x[1] for x in self.functions.items() if x[1].extensions}
 
-    def genCmd(self, cmdinfo, name):
-        pass
+    def useExtension(self, extension_elem):
+        if extension_elem.tag == 'feature':
+            return self.api == extension_elem.get('api')
+        else:
+            supported_re = re.compile('(?:' + extension_elem.get('supported') + ')$')
+            # TODO: needs to consider ES1 required vs extensions
+            return supported_re.match(self.default_extensions)
 
-    def logMsg(level, *args):
-        pass
+    def matchRequire(self, elem):
+        if 'api' in elem.attrib:
+            if elem.get('api') != self.api:
+                return False
+        if 'profile' in elem.attrib:
+            if elem.get('profile') != self.profile:
+                return False
+        return True
 
-class AliasOutputGenerator(NullOutputGenerator):
-    def genCmd(self, cmdinfo, name):
-        name = cmdinfo.elem.find('proto/name').text
-        aliases = cmdinfo.elem.findall('alias')
-        for a in aliases:
-            print('ALIAS {} {}'.format(name, a.get('name')))
+class GLAPI(API):
+    api = 'gl'
+    profile = 'compatibility'
+    default_extensions = 'gl'
 
-class TableOutputGenerator(NullOutputGenerator):
-    def __init__(
-            self,
-            errFile = sys.stderr,
-            warnFile = sys.stderr,
-            diagFile = sys.stdout):
-        self.cmd_features = {}
-        self.cur_feature = None
+def do_alias(options, apis):
+    for api in apis:
+        for function in api.functions.values():
+            aliases = function.elem.findall('alias')
+            for a in aliases:
+                print('ALIAS {} {}'.format(function.name, a.get('name')))
 
-    def beginFeature(self, interface, emit):
-        if emit:
-            self.cur_feature = interface
-
-    def endFeature(self):
-        self.cur_feature = None
-
-    def genCmd(self, cmdinfo, name):
-        # TODO: this approach assumes only one feature per command.
-        # This can't be avoided using a generator, because it only
-        # emits each command once.
-        if self.cur_feature is not None:
-            self.cmd_features[name] = self.cur_feature.get('name')
-
-def run(options, args, generator):
-    r = reg.Registry()
-    for fname in args:
-        r.loadFile(fname)
-        genOpts = reg.CGeneratorOptions(
-            filename          = 'GL/glext.h',
-            apiname           = 'gl',
-            profile           = 'compatibility',
-            defaultExtensions = 'gl',
-            sortProcedure     = sort_and_save(generator))
-        r.setGenerator(generator)
-    r.apiGen(genOpts)
-
-def do_alias(options, args):
-    run(options, args, AliasOutputGenerator())
-
-def do_header(options, args):
-    gen = NullOutputGenerator()
-    run(options, args, gen)
-
+def do_header(options, apis):
     print('/* Generated by ' + sys.argv[0] + '. Do not edit. */')
     print(dedent('''
         #ifndef BUGLE_SRC_APITABLES_H
@@ -199,9 +174,9 @@ def do_header(options, args):
         } bugle_api_function_data;
         '''))
     index = 0
-    for feature in gen.features:
-        if feature.emit:
-            print("#define BUGLE_{0} {1}".format(feature.name, index))
+    for api in apis:
+        for extension in api.extensions.values():
+            print("#define BUGLE_{0} {1}".format(extension.name, index))
             index += 1
     print("#define BUGLE_API_EXTENSION_COUNT {0}".format(index))
     print(dedent("""
@@ -211,29 +186,42 @@ def do_header(options, args):
         extern const int _bugle_api_enum_count[];
         #endif /* BUGLE_SRC_APITABLES_H */"""))
 
-def feature_descendants(feature_map, feature):
-    ans = [feature]
-    q = [feature]
+def load_function_ids(filename):
+    func_re = re.compile('^#define FUNC_(\w+)\s+(\d+)')
+    functions = []
+    with open(filename, 'r') as h:
+        for line in h:
+            match = func_re.match(line)
+            if match:
+                name = match.group(1)
+                fid = int(match.group(2))
+                if fid != len(functions):
+                    raise ValueError("Expected function ID of " + len(functions) + " but got " + fid)
+                functions.append(name)
+    return functions
+
+def extension_descendants(extensions, extension):
+    ans = [extension]
+    q = [extension]
     while q:
         cur = q.pop()
         children = genglxmltables.extension_children.get(cur.name, [])
         for c in children:
-            if c in feature_map:
-                f = feature_map[c]
+            if c in extensions:
+                f = extensions[c]
                 if f not in ans:
                     ans.append(f)
                     q.append(f)
     return ans
 
-def do_c_extension_table(gen):
-    feature_map = {x.name: x for x in gen.features}
-    for feature in gen.features:
-        if feature.emit:
-            desc = feature_descendants(feature_map, feature)
+def do_c_extension_table(apis):
+    for api in apis:
+        for extension in api.extensions.values():
+            desc = extension_descendants(api.extensions, extension)
             print(dedent('''
                 static const bugle_api_extension extension_group_{0}[] =
-                {{''').format(feature.name))
-            for d in sorted(desc, key = feature_key):
+                {{''').format(extension.name))
+            for d in sorted(desc, key = Extension.key):
                 print('    BUGLE_{0},'.format(d.name))
             print('    NULL_EXTENSION')
             print('};')
@@ -241,36 +229,39 @@ def do_c_extension_table(gen):
     print(dedent('''
         const bugle_api_extension_data _bugle_api_extension_table[] =
         {'''))
-    for feature in gen.features:
-        fields = []
-        if feature.elem.tag == 'feature':
-            fields.append('"{}"'.format(feature.number))
-        else:
-            fields.append('NULL')
-        fields.append('"{}"'.format(feature.name))
-        fields.append('extension_group_{}'.format(feature.name))
-        fields.append('BUGLE_API_EXTENSION_BLOCK_GL') # TODO
-        print('    {{ {0} }},'.format(', '.join(fields)))
+    for api in apis:
+        for extension in api.extensions.values():
+            fields = []
+            if extension.category == 'VERSION':
+                fields.append('"{}"'.format(extension.number))
+            else:
+                fields.append('NULL')
+            fields.append('"{}"'.format(extension.name))
+            fields.append('extension_group_{}'.format(extension.name))
+            fields.append('BUGLE_API_EXTENSION_BLOCK_GL') # TODO: get from api
+            print('    {{ {0} }},'.format(', '.join(fields)))
     print('};')
 
-def do_c_function_table(gen, header):
+def do_c_function_table(apis, header):
     functions = load_function_ids(header)
     print(dedent('''
         const bugle_api_function_data _bugle_api_function_table[] =
         {'''))
-    for function in functions:
-        if function not in gen.cmd_features:
-            print("WARNING: Function {} not found in registry".format(function), file = sys.stderr)
-            feature = 'NULL_EXTENSION'
+    for function_name in functions:
+        function = None
+        for api in apis:
+            if function_name in api.functions:
+                function = api.functions[function_name]
+        if function is None:
+            print("WARNING: Function {} not found in registry".format(function_name), file = sys.stderr)
+            extension = 'NULL_EXTENSION'
         else:
-            feature = 'BUGLE_' + gen.cmd_features[function]
-        print('    {{ {0} }},  /* {1} */'.format(feature, function))
+            # TODO: design currently assumes one extension per function
+            extension = 'BUGLE_' + function.extensions[0].name
+        print('    {{ {0} }},  /* {1} */'.format(extension, function_name))
     print('};')
 
-def do_c(options, args):
-    gen = TableOutputGenerator()
-    run(options, args, gen)
-
+def do_c(options, apis):
     print('/* Generated by ' + sys.argv[0] + '. Do not edit. */')
     print(dedent('''
         #if HAVE_CONFIG_H
@@ -280,8 +271,8 @@ def do_c(options, args):
         #include "apitables.h"
         '''))
 
-    do_c_extension_table(gen)
-    do_c_function_table(gen, options.header)
+    do_c_extension_table(apis)
+    do_c_function_table(apis, options.header)
 
 def main():
     parser = OptionParser()
@@ -309,7 +300,9 @@ def main():
         'header': do_header,
         'c': do_c
     }
-    modes[options.mode](options, args)
+
+    apis = [GLAPI(args[0])]
+    modes[options.mode](options, apis)
 
 if __name__ == '__main__':
     main()
